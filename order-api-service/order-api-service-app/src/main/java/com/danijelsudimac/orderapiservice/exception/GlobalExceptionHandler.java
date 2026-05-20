8ec2b6db5e037c77c5c688fae3e267ab014a32c7
Danijel Sudimac <dacha.sudimac@gmail.com>
2026-05-20 16:16:45 +0200

    refactor: Polish code

--- order-api-service/order-api-service-app/src/main/java/com/danijelsudimac/orderapiservice/exception/GlobalExceptionHandler.java
+++ order-api-service/order-api-service-app/src/main/java/com/danijelsudimac/orderapiservice/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.danijelsudimac.orderapiservice.exception;
 
 import com.danijelsudimac.orderapiservice.exception.model.ErrorResponse;
 import com.danijelsudimac.orderapiservice.exception.model.FieldError;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,13 +10,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.time.Instant;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
+
+    private static final String VALIDATION_ERROR_MESSAGE = "Validation failed for one or more fields";
+    private static final String VALIDATION_ERROR_CODE = "VALIDATION_ERROR";
+    private static final String UNEXPECTED_ERROR_MESSAGE = "An unexpected error occurred";
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationException(
@@ -26,10 +30,10 @@ public class GlobalExceptionHandler {
                 .stream()
                 .map(err -> new FieldError(err.getField(), err.getDefaultMessage()))
                 .toList();
-
+        log.warn(VALIDATION_ERROR_MESSAGE + ": {}", errors);
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(
-                        "VALIDATION_ERROR",
+                        VALIDATION_ERROR_CODE,
                         errors,
                         Instant.now()
                 ));
@@ -37,8 +41,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleAll(Exception ex) {
+        log.error(UNEXPECTED_ERROR_MESSAGE, ex);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Unexpected error");
+                .body(UNEXPECTED_ERROR_MESSAGE);
     }
 }
